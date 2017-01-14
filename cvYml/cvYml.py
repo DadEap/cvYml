1c2a0ff6413bde7539cfd489b0e5e56e2de51db2
David BOAS <david.boas@imascap.com>
2017-01-14 23:54:36 +0100

    removed class and add read_folder function

--- cvYml/cvYml.py
+++ cvYml/cvYml.py
@@ -1,24 +1,33 @@
 import yaml
 import numpy as np
-from os.path import isfile
+from os.path import isfile, isdir
+import glob
 
-class cvYml:
+def _opencv_matrix(loader, node):
+    mapping = loader.construct_mapping(node, deep=True)
+    if mapping["rows"] == 1 or mapping["cols"] == 1:
+        return mapping["data"]
+    mat = np.array(mapping["data"])
+    mat.resize(mapping["rows"], mapping["cols"])
+    return mat
 
-    @staticmethod
-    def _opencv_matrix(loader, node):
-        mapping = loader.construct_mapping(node, deep=True)
-        mat = np.array(mapping["data"])
-        mat.resize(mapping["rows"], mapping["cols"])
-        return mat
+def read_file(path):
+    if not(isfile(path) and path.lower().endswith('.yml')):
+        raise FileNotFoundError
+    yaml.add_constructor(u"tag:yaml.org,2002:opencv-matrix", _opencv_matrix)
+    with open(path) as fin:
+        for i in range(2):
+            fin.readline()
+        return yaml.load(fin.read())
 
-    def __init__(self, path):
-        if not(isfile(path) and path.lower().endswith('.yml')):
-            raise FileNotFoundError
-        self.path = path
-
-    def read(self):
-        yaml.add_constructor(u"tag:yaml.org,2002:opencv-matrix", cvYml._opencv_matrix)
-        with open(self.path) as fin:
-            for i in range(2):
-                fin.readline()
-            return yaml.load(fin.read())
+def read_folder(folder_path, file_name):
+    if not(isdir(folder_path)):
+        raise NotADirectoryError
+    path = folder_path + "/" + file_name
+    files = glob.glob(folder_path + "/" + file_name + "*.yml")
+    if not files:
+        raise FileNotFoundError
+    array = []
+    for f in files:
+        array.append(read_file(f))
+    return array
