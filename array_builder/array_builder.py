e54796f8a7065b08177309d029890db53a431ce5
David BOAS <david.boas@imascap.com>
2017-01-20 17:51:32 +0100

    Add possibility to change to first row

--- array_builder/array_builder.py
+++ array_builder/array_builder.py
@@ -1,36 +1,57 @@
-from pylatex import Tabular
+from pylatex import Tabular, LineBreak
 import numpy as np
 
-def latex_table(p_doc, p_dict, p_order = [], p_columnsName = {}, p_firstColumn = []):
+def latex_table(p_doc, p_dict, p_order = [], p_columnsName = {}, p_firstColumnName = ""):
+
     order = []
     if len(p_order) == 0:
         order = list(p_dict.keys())
     else:
         order = p_order
 
-    cols = []
+    lengthDictArrays = len(p_dict[order[0]])
+
+    cols = {}
     if len(p_columnsName) == 0:
         cols = {key : key for key in order}
     else:
-        cols = p_columnsName
+        for i in p_order:
+            if i in p_columnsName.keys():
+                cols[i] = p_columnsName[i]
+            else :
+                cols[i] = i
 
-    columnsName = []
+    columnsName = [""]
     for o in order:
         columnsName.append(cols[o])
 
+    fcol = []
+    if p_firstColumnName:
+        if p_firstColumnName in p_dict.keys():
+            fcol = p_dict[p_firstColumnName]
+        else:
+            for i in range(lengthDictArrays):
+                fcol.append(p_firstColumnName + " " + str(i+1))
+    else:
+        for i in range(lengthDictArrays):
+            fcol.append(i+1)
+
     tabCols = '|'
-    for i in range(len(order)):
+    for i in range(len(order) + 1):
         tabCols += 'c|'
 
     with p_doc.create(Tabular(tabCols)) as table:
+
         table.add_hline()
         table.add_row(columnsName)
         table.add_hline()
 
-        for n in range(len(p_dict[order[0]])):
+        for i, n in enumerate(range(lengthDictArrays)):
             table.add_hline()
-            row = []
+            row = [fcol[i]]
             for k in order:
                 row.append(p_dict[k][n])
             table.add_row(row)
     table.add_hline()
+
+    p_doc.append(LineBreak())
